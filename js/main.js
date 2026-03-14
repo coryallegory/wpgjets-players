6a27ae24934eaa215852f78d4ca68fb79bcb7a24
Cory Metcalfe <corymetcalfe@gmail.com>
2026-03-14 03:38:15 -0500

    Replace owned key toggle with mobile-friendly filter UI

--- js/main.js
+++ js/main.js
@@ -3,7 +3,14 @@ import { PlayersTable } from './table.js';
 import { bindColumnGroupToggles } from './column-groups.js';
 
 const tableElement = document.querySelector('#players-table');
+const ownedFilterElement = document.querySelector('#owned-filter');
 const playersTable = new PlayersTable(tableElement);
+
+if (ownedFilterElement) {
+  ownedFilterElement.addEventListener('change', () => {
+    playersTable.setOwnedFilter(ownedFilterElement.value);
+  });
+}
 
 playersTable.bindSorting();
 bindColumnGroupToggles(tableElement);
@@ -11,6 +18,10 @@ bindColumnGroupToggles(tableElement);
 loadPlayers()
   .then((players) => {
     playersTable.setPlayers(players);
+
+    if (ownedFilterElement) {
+      playersTable.setOwnedFilter(ownedFilterElement.value);
+    }
   })
   .catch((error) => {
     playersTable.renderError(error.message);
